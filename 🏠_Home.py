e97525582bb35fac32c50f517803eca9b595313c
Mochammad Lanang Afkaar Arrachmandhika <mochammad.dhika@primecash.co.id>
2023-08-05 18:45:14 +0700

    update home and datasource

--- 🏠_Home.py
+++ 🏠_Home.py
@@ -1,29 +1,55 @@
 from common import BaseInterface
+
 import streamlit as st
+from sqlalchemy import create_engine, MetaData, text
+from sqlalchemy.orm import sessionmaker
+import pandas as pd
+from pandasai import p
 
-from sqlalchemy import create_engine, MetaData, Table
-from sqlalchemy.engine.url import make_url
+class DataSource:
+    def __init__(self) -> None:
+        DATABASE_URL = "sqlite:///datasets.db"
+        self.engine = create_engine(DATABASE_URL)
 
-DATABASE_URL = "sqlite:///datasets.db"
-engine = create_engine(DATABASE_URL)
+        metadata = MetaData()
+        metadata.reflect(bind=self.engine)
 
-metadata = MetaData()
-metadata.reflect(bind=engine)
+        self.table_list = list(metadata.tables.keys())
+        
+    def getData(self, SELECTED_TABLE):
+        Session = sessionmaker(bind=self.engine)
+        session = Session()
 
-table_list = list(metadata.tables.keys())
+        selected_table = text(f"SELECT * FROM {SELECTED_TABLE}")
+
+        result = session.execute(selected_table).fetchall()
+        return pd.DataFrame(result)
+
+    def getHeadData(self, SELECTED_TABLE):
+        return self.getData(SELECTED_TABLE).head(5)
+    
+class LLM:
+    def __init__(self) -> None:
+        pass
+
 
 class HomeInterface(BaseInterface):
     def __init__(self) -> None:
         pass
-
+    
     def userInput(self):
-        col1, col2 = st.columns(spec=[0.7, 0.2], gap="medium")
+        dataSource = DataSource()
+        dfShow = st.empty()
+        col1, col2 = st.columns(spec=[0.7, 0.3], gap="medium")
         with col1:
             promptText = st.text_area(label="Input your Prompt:")
         with col2:
-            dataSource = st.selectbox(label="Select data source Table:", options=table_list)
+            selectedSource = st.selectbox(label="Select data source Table:", options=dataSource.table_list)
+        
+        st.dataframe(data=DataSource().getHeadData(selectedSource), use_container_width=True)
 
     def main(self):
+        st.title("Data Analyst Assistant")
         self.sideBar()
         with st.container():
             self.userInput()
